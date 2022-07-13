abf77fca07981e754c85cd17a62d79735f8ee433
Aldo Rodrigo Hernandez <aldo.hernandez@cmgo.org.mx>
2022-07-13 16:33:56 -0500

    init course

---
+++ introduction.js
@@ -0,0 +1,8 @@
+/*
+    JavaScript was created by Brendan Eich, went his worked in Netscape.
+
+    JavaScript is different to Java (Some people thinks both things are the same ) 
+
+    Mocha and liveScript were it´s names before "JavaScript"
+
+*/
