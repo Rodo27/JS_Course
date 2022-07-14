89ffe33d3473631ce8a382f7954db76e3f51b236
Aldo Rodrigo <aldo.hernandez@testjjsolucionesempresariales.com>
2022-07-14 03:51:25 -0500

    grammar and features

---
+++ features_and_grammar.js
@@ -0,0 +1,65 @@
+/*
+
+	Features and grammar
+	
+	
+	Isomorphism
+	
+	Frontend 
+	Backend 
+	DB
+
+	other:
+		Moviles
+		Web Developer
+		Machine Learning
+		
+		
+	Structure 
+	
+	Use snake_case in files :
+		my_file.js
+		
+	Use UPPER_CASE in consts:
+		PI = 3.1416
+	
+	Use UpperCamelCase in class:
+		class NewObjectClass{
+			construc (){
+			}
+		}
+
+	Use lowerCamelCase in Objects, primitives, functions:
+		const objectNew = {
+			name : 'Aldo',
+			email : 'aldo1rodrigo@gmail.com'
+		}
+		
+	
+	
+	
+	Code Flow 
+	
+		1 import modules
+		2 declarate variables
+		3 declarete functions
+		4 write code
+		
+		
+	Types of data
+	
+	Primitives 
+		string
+		number
+		boolean
+		null
+		undefined
+		NaN
+		
+	Complex	
+		object
+		array
+		function
+		class
+		
+*/
