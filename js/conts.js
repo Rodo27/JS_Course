104ef9a3168509848697b91a7fd8e4a2f87e3173
Aldo Rodrigo Hernandez <aldo.hernandez@cmgo.org.mx>
2022-08-17 16:56:44 -0500

    add modules.js

---
+++ js/conts.js
@@ -0,0 +1,4 @@
+export const PI = Math.PI
+
+export let user = "Aldo Rodrigo"
+let password = "123456"
--- js/modules.js
+++ js/modules.js
@@ -1,1 +1,9 @@
+import {PI, user} from "./conts.js"
+import {arithmetic} from "./arithmetic.js"
+
 console.log("File modules.js")
+
+console.log(PI, user)
+
+console.log(arithmetic.sumar(15,25))
+console.log(arithmetic.restar(15,25))
